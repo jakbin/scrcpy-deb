aa90c048a3961c7e1c86503c0cd76616a107e54b
jakbin <jakbin4747@gmail.com>
2024-10-18 21:38:42 +0530

    setup

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # scrcpy-deb
-deb file scrcpy tool
+
+[![All Downloads](https://img.shields.io/github/downloads/jakbin/scrcpy-deb/total.svg)](https://github.com/jakbin/scrcpy-deb)
+
+deb files scrcpy tool
---
+++ build.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+git clone https://github.com/Genymobile/scrcpy scrcpy-git
+cd scrcpy-git
+sed -i '21d' install_release.sh
+./install_release.sh
+
+cd ..
+
+# copy control file
+mkdir scrcpy
+cp -r DEBIAN scrcpy/
+
+# copy share folder
+mkdir scrcpy/usr
+cp -r share scrcpy/usr/
+
+# copy scrcpy binary
+mkdir scrcpy/bin
+cp scrcpy-git/build-auto/app/scrcpy scrcpy/bin/
+
+# copy scrcpy server file
+mkdir -p scrcpy/usr/local/share/scrcpy/
+cp scrcpy-git/scrcpy-server scrcpy/usr/local/share/scrcpy/ 
+
+# build deb file
+dpkg -b scrcpy
